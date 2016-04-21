88dd84d2fd8ea9c05104fbea382522e9af3e4b06
igor <igor@hdivsecurity.com>
2016-04-21 14:29:35 +0200

    Expose Id for Category entity

--- src/main/java/sample/halforms/JacksonCustomizations.java
+++ src/main/java/sample/halforms/JacksonCustomizations.java
@@ -5,12 +5,15 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.hateoas.RelProvider;
 import org.springframework.hateoas.hal.CurieProvider;
 import org.springframework.hateoas.hal.Jackson2HalFormsModule;
 import org.springframework.hateoas.hal.Jackson2HalFormsModule.HalFormsHandlerInstantiator;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import sample.halforms.model.Category;
 
 @Configuration
 public class JacksonCustomizations {
@@ -27,6 +30,9 @@ public class JacksonCustomizations {
 	@Autowired
 	private MessageSourceAccessor resourceDescriptionMessageSourceAccessor;
 
+	@Autowired
+	private RepositoryRestConfiguration restConfig;
+
 	@PostConstruct
 	public void addHalFormsModule() {
 
@@ -34,5 +40,6 @@ public class JacksonCustomizations {
 		halObjectMapper.setHandlerInstantiator(new HalFormsHandlerInstantiator(relProvider, curieProvider,
 				resourceDescriptionMessageSourceAccessor, true));
 
+		restConfig.exposeIdsFor(Category.class);
 	}
 }
